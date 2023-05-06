8ff7990f72e96a5266e19cabcabfdd031f83429a
OKigarOK <91433816+OKigarOK@users.noreply.github.com>
2023-05-06 17:36:35 +0300

    Add files via upload

--- data/useful.js
+++ data/useful.js
@@ -1,30 +1,5 @@
 export const USEFUL = [
     {
-        title: 'Объемы технических жидкостей:',
-        images_links: ['./images/useful/fluid_capasities.jpg'],
-        about: ''
-    },
-    {
-        title: 'Параметры клиренса в стоке:',
-        images_links: ['./images/useful/clearance.jpg'],
-        about: ''
-    },
-    {
-        title: 'Передние проставки (чертеж):',
-        images_links: ['./images/useful/front_spacers.jpg'],
-        about: ''
-    },
-    {
-        title: 'Все сальники раздатки AWD:',
-        images_links: ['./images/useful/awd_seals.jpg'],
-        about: ''
-    },
-    {
-        title: 'Вал привода замка зажигания:',
-        images_links: ['./images/useful/switch_actuator.gif'],
-        about: ''
-    },
-    {
         title: '"Болванки" в заднюю балку замест сайлентлоков (чертежи)',
         images_links: [
             './images/useful/front_bushing_detail.jpg',
@@ -34,51 +9,18 @@ export const USEFUL = [
         about: 'Чертежи можно скачать в "КАЧАЛКЕ"'
     },
     {
-        title: 'Моменты затяжки передней подвески:',
-        images_links: ['./images/useful/tightening_torques_front_suspension.jpg'],
+        title: 'Вал привода замка зажигания:',
+        images_links: ['./images/useful/switch_actuator.gif'],
         about: ''
     },
     {
-        title: 'Моменты затяжки задней подвески:',
-        images_links: ['./images/useful/tightening_torques_rear_suspension.jpg'],
+        title: 'Все сальники раздатки AWD:',
+        images_links: ['./images/useful/awd_seals.jpg'],
         about: ''
     },
     {
-        title: 'Типы применяемых ламп:',
-        images_links: ['./images/useful/lamps.jpg'],
-        about: ''
-    },
-    {
-        title: 'Размеры сайлентблоков рычагов задней подвески (для "задротов"):',
-        images_links: [
-            './images/useful/Poly1.jpg',
-            './images/useful/Poly2.jpg',
-        ],
-        about: ''
-    },
-    {
-        title: 'Уровень масла АКПП в зависимости от температуры:',
-        images_links: ['./images/useful/automatic_transmission_oil_level.jpg'],
-        about: ''
-    },
-    {
-        title: 'Размеры "морды" кузова (для кузовщиков):',
-        images_links: ['./images/useful/body_dimensions_1.jpg'],
-        about: ''
-    },
-    {
-        title: 'Размеры всего кузова (для кузовщиков):',
-        images_links: ['./images/useful/body_dimensions_2.jpg'],
-        about: ''
-    },
-    {
-        title: 'Распиновка штатной магнитолы:',
-        images_links: ['./images/useful/car_radio_pinout.jpg'],
-        about: ''
-    },
-    {
-        title: 'Мини-катализатор своими руками:',
-        images_links: ['./images/useful/handmade_catalyst.jpg'],
+        title: 'Давление масла в системе',
+        images_links: ['./images/useful/oil_pressure.jpg'],
         about: ''
     },
     {
@@ -92,8 +34,18 @@ export const USEFUL = [
         about: ''
     },
     {
-        title: 'Система улавливания паров (EVAP):',
-        images_links: ['./images/useful/vapor_recovery_system.jpg'],
+        title: 'Мини-катализатор своими руками:',
+        images_links: ['./images/useful/handmade_catalyst.jpg'],
+        about: ''
+    },
+    {
+        title: 'Моменты затяжки задней подвески:',
+        images_links: ['./images/useful/tightening_torques_rear_suspension.jpg'],
+        about: ''
+    },
+    {
+        title: 'Моменты затяжки передней подвески:',
+        images_links: ['./images/useful/tightening_torques_front_suspension.jpg'],
         about: ''
     },
     {
@@ -102,8 +54,8 @@ export const USEFUL = [
         about: ''
     },
     {
-        title: 'Давление масла в системе',
-        images_links: ['./images/useful/oil_pressure.jpg'],
+        title: 'Объемы технических жидкостей:',
+        images_links: ['./images/useful/fluid_capasities.jpg'],
         about: ''
     },
     {
@@ -111,10 +63,58 @@ export const USEFUL = [
         images_links: ['./images/useful/bleed_the_brakes.jpg'],
         about: ''
     },
-
     {
-        title: '',
-        images_links: ['./images/useful/'],
+        title: 'Параметры клиренса в стоке:',
+        images_links: ['./images/useful/clearance.jpg'],
         about: ''
     },
+    {
+        title: 'Передние проставки (чертеж):',
+        images_links: ['./images/useful/front_spacers.jpg'],
+        about: ''
+    },
+    {
+        title: 'Размеры всего кузова (для кузовщиков):',
+        images_links: ['./images/useful/body_dimensions_2.jpg'],
+        about: ''
+    },
+    {
+        title: 'Размеры "морды" кузова (для кузовщиков):',
+        images_links: ['./images/useful/body_dimensions_1.jpg'],
+        about: ''
+    },
+    {
+        title: 'Размеры сайлентблоков рычагов задней подвески (для "задротов"):',
+        images_links: [
+            './images/useful/Poly1.jpg',
+            './images/useful/Poly2.jpg',
+        ],
+        about: ''
+    },
+    {
+        title: 'Распиновка штатной магнитолы:',
+        images_links: ['./images/useful/car_radio_pinout.jpg'],
+        about: ''
+    },
+    {
+        title: 'Система улавливания паров (EVAP):',
+        images_links: ['./images/useful/vapor_recovery_system.jpg'],
+        about: ''
+    },
+    {
+        title: 'Типы применяемых ламп:',
+        images_links: ['./images/useful/lamps.jpg'],
+        about: ''
+    },
+    {
+        title: 'Уровень масла АКПП в зависимости от температуры:',
+        images_links: ['./images/useful/automatic_transmission_oil_level.jpg'],
+        about: ''
+    },
+
+    // {
+    //     title: '',
+    //     images_links: ['./images/useful/'],
+    //     about: ''
+    // },
 ]
